7aefd2ef3418364fa7148de8a1a608cb2addb5ba
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2021-06-12 11:02:28 -0500

    Added insert logic

--- linkedLists/customLinkedList.js
+++ linkedLists/customLinkedList.js
@@ -50,16 +50,80 @@ class LinkedList {
         return this;
     }
 
+    _traverseToIndex(index){
+        let i =0;
+        let currentNode = this.head;
+        while(currentNode != null && i < index-1){
+            currentNode = currentNode.next;
+            i++;
+        }
+
+        return currentNode;
+    }
+
+    insert(index, value){
+        // check if index is in bound using length
+        const isBound = (index < this.length && index > 0);
+
+        // if bound then find the index node -> find & store next in temp -> set next to new node -> set next of newNode to the rest
+        if(isBound) {
+            const currentNode = this._traverseToIndex(index);
+
+            // Not that we current node, insert
+            const newNode = this._createNode(value);
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+            this.length += 1;
+        } else {
+            // append to end?
+            if(index <= 0){
+                this.prepend(value);
+            } else {
+                this.append(value);
+            }
+        }
+
+        return this;
+    }
+
     _createNode(val){
         return new LinkedListNode(val);
+    }
+
+    toArray(){
+        const arr = [];
+        let currentNode = this.head;
+
+        while (currentNode !== null){
+            arr.push(currentNode.value);
+
+            // Traverse to next
+            currentNode = currentNode.next;
+        }
+        return arr;
     }
 }
 
 const myLinkedList = new LinkedList(10);
+console.log(JSON.stringify(myLinkedList.toArray()));
+
 myLinkedList.append(5);
+console.log(JSON.stringify(myLinkedList.toArray()));
+
 myLinkedList.append(16);
+console.log(JSON.stringify(myLinkedList.toArray()));
+
 myLinkedList.append(20);
+console.log(JSON.stringify(myLinkedList.toArray()));
+
 myLinkedList.prepend(1);
+console.log(JSON.stringify(myLinkedList.toArray()));
+
 myLinkedList.prepend(3);
+console.log(JSON.stringify(myLinkedList.toArray()));
+
+// Whe have 3-10-5-20 which goes to 3-10-99-5-20
+myLinkedList.insert(2, 99)
+console.log(JSON.stringify(myLinkedList.toArray()));
 
 console.log('Linked list value:', JSON.stringify(myLinkedList));
